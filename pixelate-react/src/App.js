4c1b57fa501056c9174eb08ee5f601c8f2e45817
Beatrice Tan <joanb.tan@gmail.com>
2023-05-15 21:11:33 -0400

    install mutler

--- pixelate-react/src/App.js
+++ pixelate-react/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Footer from './components/Footer';
 // import {useState, useEffect} from "react";
 import Header from "./components/Header";
+import Upload from "./components/Upload";
 
 
 function App() {
   return (
     <div className="page">
       <Header />
+      <Upload />
       <Footer />
     </div>
   );
---
+++ pixelate-react/src/components/Upload.js
@@ -0,0 +1,8 @@
+export default function upload() {
+    return (
+       <section id="upload">
+            <h3>Upload your image</h3>
+            {/* code goes here  */}
+       </section>
+    );   
+   }
