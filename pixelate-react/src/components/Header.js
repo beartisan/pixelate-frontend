e2cb7556b81035aa5d83da51e5bbee2ec71e8a2e
Beatrice Tan <joanb.tan@gmail.com>
2023-06-11 00:55:18 -0400

    update header code

--- pixelate-react/src/components/Header.js
+++ pixelate-react/src/components/Header.js
@@ -1,32 +1,33 @@
 import React from "react";
 
-function Header() {
-  return (
-    <header id="header">
-      <h2 id="site-name">
-        <a href="/">Pixelate</a>
-      </h2>
-      <nav>
-        <ul id="navbar" className="navbar">
-          <li className="nav-item">
-            <a href="#sec-about">About BRICKMMO</a>
-          </li>
-          <li className="nav-item">
-            <a href="#sec-purchase">Buy LEGO</a>
-          </li>
-          <li className="nav-item">
-            <a href="#sec-socmed"><i class="fa-brands fa-twitter"></i> </a>
-          </li>
-          <li className="nav-item">
-            <a href="#sec-socmed"><i class="fa-brands fa-instagram"></i></a>
-          </li>
-          <li className="nav-item">
-            <a href="#sec-socmed"><i class="fa-brands fa-tiktok"></i></a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+    function Header() {
+      return (
+        <header id="header">
+          <h2 id="site-name">
+            <a href="/">Pixelate</a>
+          </h2>
+          <nav>
+            <ul id="navbar" className="navbar">
+              <li className="nav-item">
+                <a href="#sec-about">About BRICKMMO</a>
+              </li>
+              <li className="nav-item">
+                <a href="#sec-purchase">Buy LEGO</a>
+              </li>
+              <li className="nav-item">
+                <a href="#sec-socmed"><i class="fa-brands fa-twitter"></i> </a>
+              </li>
+              <li className="nav-item">
+                <a href="#sec-socmed"><i class="fa-brands fa-instagram"></i></a>
+              </li>
+              <li className="nav-item">
+                <a href="#sec-socmed"><i class="fa-brands fa-tiktok"></i></a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      );
+    }
+    
 
 export default Header;
