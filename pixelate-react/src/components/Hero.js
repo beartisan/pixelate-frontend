1b97f18bd489331c5c794f4cc169c9e0b071a143
Beatrice Tan <joanb.tan@gmail.com>
2023-06-11 00:54:28 -0400

    fix hero

--- pixelate-react/src/components/Hero.js
+++ pixelate-react/src/components/Hero.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import heroPhoto from '../vangogh-brick.jpeg';
 
-export default function Hero() {
-    return (
-        <section id="hero">
-            <div class="section-container">
-                <div class="hero-texts">
-                <h2 class="hero-title">Lego-lize your photos!</h2>
-                <p class="hero-text">Transform your uploaded images into LEGO shuds for free!</p>
 
-                <p class="hero-text">Start building your unique creations today!
-                </p>
-                </div>
-                <img src={heroPhoto} alt="Van Gogh in Lego Bricks" class="hero-image"/>
-            </div>
-        </section>
-    );   
-   }
+function Hero() {
+  return (
+    <section id="hero">
+      <div class="section-container">
+        <div class="hero-texts">
+          <h2 class="hero-title">Lego-ize your photos!</h2>
+          <p class="hero-text">
+            Transform your uploaded images into LEGO shuds for free!
+          </p>
+
+          <p class="hero-text">Start building your unique creations today!</p>
+        </div>
+        <img
+          src="../images/vangogh-brick.jpeg"
+          alt="Van Gogh in Lego Bricks"
+          class="hero-image"
+        />
+      </div>
+    </section>
+  );
+}
+  
+  export default Hero;
+  
